3ca3380b693eb6c0f6853d861e57224ef3f318f7
novacek <novacek@69b9e6ae-700c-0410-834d-e0df161635a7>
2011-09-21 21:23:48 +0000

    Fixed critical NeOn Plugin bug
    git-svn-id: https://svn.fzi.de/svn/ipesvn/code/trunk/Theseus/CTC/Replica@49384 69b9e6ae-700c-0410-834d-e0df161635a7

--- bundles/de.fzi.replica.neonplugin/src/de/fzi/replica/neonplugin/commands/ReplicaProjectFactory.java
+++ bundles/de.fzi.replica.neonplugin/src/de/fzi/replica/neonplugin/commands/ReplicaProjectFactory.java
@@ -1,9 +1,13 @@
 package de.fzi.replica.neonplugin.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IProject;
 import org.neontoolkit.core.IOntologyProjectFactory;
 import org.neontoolkit.core.NeOnCorePlugin;
 import org.neontoolkit.core.project.IOntologyProject;
+import org.semanticweb.owlapi.model.OWLOntologyFactory;
 
 import de.fzi.replica.neonplugin.Activator;
 
@@ -11,10 +15,13 @@ public class ReplicaProjectFactory implements IOntologyProjectFactory {
 	public static final String FACTORY_ID = "de.fzi.replica.neonplugin.commands.ReplicaProjectFactory"; //$NON-NLS-1$
     public static final String ONTOLOGY_LANGUAGE = "OWL2"; //$NON-NLS-1$
     
+    private List<OWLOntologyFactory> thirdPartyFactories = new ArrayList<OWLOntologyFactory>();
+    
 	@Override
 	public IOntologyProject createOntologyProject(IProject project) {
 		Activator.getDefault().logInfo("createOntologyProject("+project+")");
 		ReplicaProject ontologyProject = new ReplicaProject(project.getName());
+		ontologyProject.setThirdPartyFactories(thirdPartyFactories);
         NeOnCorePlugin.getDefault().updateMarkers(ontologyProject);
 		return ontologyProject;
 	}
