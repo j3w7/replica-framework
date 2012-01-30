6f93e86b43f0d527558d21bb79da5c115c13c626
novacek <novacek@69b9e6ae-700c-0410-834d-e0df161635a7>
2012-01-30 11:24:46 +0000

    git-svn-id: https://svn.fzi.de/svn/ipesvn/code/trunk/Theseus/CTC/Replica@54195 69b9e6ae-700c-0410-834d-e0df161635a7

--- test/bundles/de.fzi.replica.app.test/src/de/fzi/replica/app/test/ClientTestCase.java
+++ test/bundles/de.fzi.replica.app.test/src/de/fzi/replica/app/test/ClientTestCase.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2011 Jan Novacek
+   Copyright 2012 Jan Novacek
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -16,8 +16,8 @@
 
 package de.fzi.replica.app.test;
 
-import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -26,18 +26,25 @@ import junit.framework.Assert;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.semanticweb.owlapi.model.AddAxiom;
 import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLException;
 import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyChange;
+import org.semanticweb.owlapi.model.OWLOntologyChangeListener;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyID;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 import de.fzi.replica.app.client.Client.AddException;
+import de.fzi.replica.app.client.Client.AsyncMethodCallback.Result;
 import de.fzi.replica.app.client.Client.FetchException;
 import de.fzi.replica.app.client.Client.OnGroupsReceivedListener;
 import de.fzi.replica.app.client.Client.OnOntologyAddedListener;
 import de.fzi.replica.app.client.Client.OnOntologyIDsReceivedListener;
 import de.fzi.replica.app.client.Client.OnOntologyReceivedListener;
-import de.fzi.replica.app.client.Client.AsyncMethodCallback.Result;
 
 /**
  * Tests all methods of the <code>Client</code> interface.
@@ -45,7 +52,7 @@ import de.fzi.replica.app.client.Client.AsyncMethodCallback.Result;
  * @see de.fzi.replica.app.client.Client
  * 
  * @author Jan Novacek novacek@fzi.de, jannvck@gmail.com
- * @version 0.1, 20.07.2011
+ * @version 0.2, 25.01.2012
  */
 public class ClientTestCase extends AbstractApplicationTestCase {
 
@@ -83,9 +90,9 @@ public class ClientTestCase extends AbstractApplicationTestCase {
 		Result result1;
 		Object[] objs;
 	}
-	
+
 	final Holder h = new Holder();
-	
+
 	class AddOntoThread extends Thread {
 		@Override
 		public void run() {
@@ -120,7 +127,7 @@ public class ClientTestCase extends AbstractApplicationTestCase {
 			}
 		}
 	}
-	
+
 	@Test
 	public void testAddOntology() throws OWLOntologyCreationException,
 			AddException, InterruptedException {
@@ -132,21 +139,131 @@ public class ClientTestCase extends AbstractApplicationTestCase {
 		assertEquals(Result.OK, h.result1);
 		System.out.println("------testAddOntology()------end\n");
 	}
-	
-	class GetGroupsThread extends Thread {
+
+	class AddOntoAndModifyThread extends Thread {
 		@Override
 		public void run() {
 			try {
-				clearHolder();
+				// clear the holder
+				h.result0 = null;
+				h.result1 = null;
 				h.objs = new Object[2];
-				getClient(A).getGroups(new OnGroupsReceivedListener() {
+				h.objs[0] = null;
+				h.objs[1] = null;
+
+				final OWLOntology onto0 = createTestOWLOntology(ontologyID0);
+				onto0.getOWLOntologyManager().removeAxioms(onto0,
+						onto0.getAxioms());
+				Set<String> groups = new HashSet<String>();
+				groups.add("testgroup0");
+				groups.add("testgroup1");
+				groups.add("testgroup2");
+				getClient(A).addOWLOntology(onto0, groups,
+						new OnOntologyAddedListener() {
+							@Override
+							public void onOntologyAdded(Result result) {
+								h.result0 = result;
+								h.objs[0] = onto0;
+							}
+						});
+				Thread.sleep(500);
+				getClient(B).getOWLOntology(ontologyID0,
+						new OnOntologyReceivedListener() {
+							@Override
+							public void onOntologyReceived(Result result,
+									OWLOntology onto) {
+								h.result1 = result;
+								h.objs[1] = onto;
+							}
+						});
+
+				while (h.objs[1] == null) {
+					// wait
+				}
+
+				/*
+				 * Modify the ontology
+				 */
+
+				OWLOntologyManager man0 = onto0.getOWLOntologyManager();
+				man0.addOntologyChangeListener(new OWLOntologyChangeListener() {
 					@Override
-					public void onGroupsGot(Result result, Set<String> groups) {
-						h.result0 = result;
-						h.objs[0] = groups;
+					public void ontologiesChanged(
+							List<? extends OWLOntologyChange> changes)
+							throws OWLException {
+						System.out.println("onto0 changes: " + changes);
 					}
 				});
-				Thread.sleep(100);
+
+				((OWLOntology) h.objs[0]).getOWLOntologyManager()
+						.addOntologyChangeListener(
+								new OWLOntologyChangeListener() {
+									@Override
+									public void ontologiesChanged(
+											List<? extends OWLOntologyChange> changes)
+											throws OWLException {
+										System.out.println("onto1 changes: "
+												+ changes);
+									}
+								});
+
+				// Now modifiy ontology 0, later watch out for changes on onto 1
+				OWLDataFactory fac = man0.getOWLDataFactory();
+				OWLClass clA = fac.getOWLClass(IRI.create("classA"));
+				OWLClass clB = fac.getOWLClass(IRI.create("classB"));
+				man0.applyChange(new AddAxiom(onto0, fac.getOWLSubClassOfAxiom(
+						clA, clB)));
+			} catch (AddException e) {
+				e.printStackTrace();
+			} catch (OWLOntologyCreationException e) {
+				e.printStackTrace();
+			} catch (FetchException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Test
+	public void testAddOntologyAndModify()
+		throws OWLOntologyCreationException,
+			AddException, InterruptedException {
+		System.out.println("------testAddOntologyAndModify()------start");
+		AddOntoAndModifyThread t = new AddOntoAndModifyThread();
+		t.start();
+		Thread.sleep(8000);
+		assertEquals(Result.OK, h.result0);
+		assertEquals(Result.OK, h.result1);
+		assertEquals(((OWLOntology) h.objs[0]).getAxioms(),
+				((OWLOntology) h.objs[1]).getAxioms());
+		System.out.println("------testAddOntologyAndModify()------end\n");
+	}
+
+	class AddOntoAndCheckGroupsThread extends Thread {
+		@Override
+		public void run() {
+			try {
+				// clear the holder
+				h.result0 = null;
+				h.result1 = null;
+				h.objs = new Object[2];
+
+				final OWLOntology onto0 = createTestOWLOntology(ontologyID0);
+				onto0.getOWLOntologyManager().removeAxioms(onto0,
+						onto0.getAxioms());
+				Set<String> groups = new HashSet<String>();
+				groups.add("testgroup0");
+				groups.add("testgroup1");
+				groups.add("testgroup2");
+				getClient(A).addOWLOntology(onto0, groups,
+						new OnOntologyAddedListener() {
+							@Override
+							public void onOntologyAdded(Result result) {
+								h.result0 = result;
+								h.objs[0] = onto0;
+							}
+						});
 				getClient(B).getGroups(new OnGroupsReceivedListener() {
 					@Override
 					public void onGroupsGot(Result result, Set<String> groups) {
@@ -154,43 +271,152 @@ public class ClientTestCase extends AbstractApplicationTestCase {
 						h.objs[1] = groups;
 					}
 				});
-			} catch (InterruptedException e) {
+				// getClient(B).getOWLOntology(ontologyID0,
+				// new OnOntologyReceivedListener() {
+				// @Override
+				// public void onOntologyReceived(Result result,
+				// OWLOntology onto) {
+				// h.result1 = result;
+				// h.objs[1] = onto;
+				// }
+				// });
+
+				while (h.objs[1] == null) {
+					// wait
+				}
+
+			} catch (AddException e) {
+				e.printStackTrace();
+			} catch (OWLOntologyCreationException e) {
 				e.printStackTrace();
 			} catch (FetchException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
+	@Test
+	public void testAddOntologyAndCheckGroups()
+			throws OWLOntologyCreationException, AddException,
+			InterruptedException {
+		System.out.println("------testAddOntologyAndModify()------start");
+		AddOntoAndCheckGroupsThread t = new AddOntoAndCheckGroupsThread();
+		t.start();
+		Thread.sleep(8000);
+		assertEquals(Result.OK, h.result0);
+		assertEquals(Result.OK, h.result1);
+		// assertEquals(((OWLOntology) h.objs[0]).getAxioms(),
+		// ((OWLOntology) h.objs[1]).getAxioms());
+		System.out.println("------testAddOntologyAndModify()------end\n");
+	}
+
+//	class GetGroupsThread extends Thread {
+//		@Override
+//		public void run() {
+//			try {
+//				clearHolder();
+//				h.objs = new Object[2];
+//				getClient(A).getGroups(new OnGroupsReceivedListener() {
+//					@Override
+//					public void onGroupsGot(Result result, Set<String> groups) {
+//						h.result0 = result;
+//						h.objs[0] = groups;
+//					}
+//				});
+//				Thread.sleep(100);
+//				getClient(B).getGroups(new OnGroupsReceivedListener() {
+//					@Override
+//					public void onGroupsGot(Result result, Set<String> groups) {
+//						h.result1 = result;
+//						h.objs[1] = groups;
+//					}
+//				});
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			} catch (FetchException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//	}
+
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testGetGroups() throws FetchException {
 		System.out.println("------testGetGroups()------start");
 		clearHolder();
 		h.objs = new Object[2];
-		getClient(A).getGroups(new OnGroupsReceivedListener() {
-			@Override
-			public void onGroupsGot(Result result, Set<String> groups) {
-				h.result0 = result;
-				h.objs[0] = groups;
-			}
-		});
-		getClient(B).getGroups(new OnGroupsReceivedListener() {
-			@Override
-			public void onGroupsGot(Result result, Set<String> groups) {
-				h.result1 = result;
-				h.objs[1] = groups;
-			}
-		});
+		h.objs[0] = null;
+		h.objs[1] = null;
+		OWLOntology onto0 = null;
+		try {
+			onto0 = createTestOWLOntology(ontologyID0);
+		} catch (OWLOntologyCreationException e) {
+			e.printStackTrace();
+		}
+		onto0.getOWLOntologyManager().removeAxioms(onto0,
+				onto0.getAxioms());
+		Set<String> groups0 = new HashSet<String>();
+		groups0.add("test groups0 0");
+		groups0.add("test groups0 1");
+		groups0.add("test groups0 2");
+		Set<String> groups1 = new HashSet<String>();
+		groups1.add("test groups1 0");
+		groups1.add("test groups1 1");
+		groups1.add("test groups1 2");
+		try {
+			getClient(A).addOWLOntology(onto0,
+					groups0, new OnOntologyAddedListener() {
+				@Override
+				public void onOntologyAdded(Result result) {
+					assertEquals(result, Result.OK);
+				}
+			});
+			Thread.sleep(500);
+			getClient(B).addOWLOntology(onto0,
+					groups1, new OnOntologyAddedListener() {
+				@Override
+				public void onOntologyAdded(Result result) {
+					assertEquals(result, Result.OK);
+				}
+			});
+			Thread.sleep(500);
+			getClient(A).getGroups(new OnGroupsReceivedListener() {
+				@Override
+				public void onGroupsGot(Result result, Set<String> groups) {
+					h.result0 = result;
+					h.objs[0] = groups;
+				}
+			});
+			Thread.sleep(500);
+			getClient(B).getGroups(new OnGroupsReceivedListener() {
+				@Override
+				public void onGroupsGot(Result result, Set<String> groups) {
+					h.result1 = result;
+					h.objs[1] = groups;
+				}
+			});
+		} catch (AddException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 		sleep(200);
 		assertEquals(Result.OK, h.result0);
 		assertEquals(Result.OK, h.result1);
 		assertNotNull(h.objs[0]);
 		assertNotNull(h.objs[1]);
+		for(String s : groups0) {
+			assertTrue(((Set<String>) h.objs[0]).contains(s));
+		}
+		for(String s : groups1) {
+			assertTrue(((Set<String>) h.objs[1]).contains(s));
+		}
 		System.out.println("h.objs[0]=" + h.objs[0]);
 		System.out.println("h.objs[1]=" + h.objs[1]);
 		System.out.println("------testGetGroups()------end\n");
 	}
-	
+
 	class GetOntoThread extends Thread {
 		@Override
 		public void run() {
@@ -221,7 +447,7 @@ public class ClientTestCase extends AbstractApplicationTestCase {
 			}
 		}
 	}
-	
+
 	@Test
 	public void testGetOntologyAndFail() throws InterruptedException,
 			FetchException {
@@ -235,7 +461,7 @@ public class ClientTestCase extends AbstractApplicationTestCase {
 		assertEquals(Result.NULL, h.result1);
 		System.out.println("------testGetOntologyAndFail()------end\n");
 	}
-	
+
 	@Test
 	public void testGetOntology() throws InterruptedException, FetchException,
 			OWLOntologyCreationException, AddException {
@@ -254,55 +480,60 @@ public class ClientTestCase extends AbstractApplicationTestCase {
 		System.out.println("h.objs[1]=" + h.objs[1]);
 		System.out.println("------testGetOntology()------end\n");
 	}
-	
+
 	class GetOntoIDsThread extends Thread {
 		@Override
 		public void run() {
 			try {
-				System.out.println("creating test ontology, ontologyID="+ontologyID0);
+				System.out.println("creating test ontology, ontologyID="
+						+ ontologyID0);
 				OWLOntology onto0 = createTestOWLOntology(ontologyID0);
-				getClient(A).addOWLOntology(onto0, Collections.singleton("testgroup0"),
+				Set<String> groups = new HashSet<String>();
+				groups.add("testgroup0");
+				groups.add("testgroup1");
+				groups.add("testgroup2");
+				getClient(A).addOWLOntology(onto0, groups,
 						new OnOntologyAddedListener() {
 							@Override
 							public void onOntologyAdded(Result result) {
 								Assert.assertEquals(Result.OK, result);
-								System.out.println("onOntologyAdded("+result+")");
+								System.out.println("onOntologyAdded(" + result
+										+ ")");
 							}
 						});
 				Thread.sleep(1000);
 				OWLOntology onto1 = createTestOWLOntology(ontologyID1);
-				getClient(B).addOWLOntology(onto1, Collections.singleton("testgroup0"),
+				getClient(B).addOWLOntology(onto1, groups,
 						new OnOntologyAddedListener() {
 							@Override
 							public void onOntologyAdded(Result result) {
 								Assert.assertEquals(Result.OK, result);
-								System.out.println("onOntologyAdded("+result+")");
+								System.out.println("onOntologyAdded(" + result
+										+ ")");
 							}
 						});
 				Thread.sleep(2000);
-				Set<Object> groups = new HashSet<Object>();
-				groups.add("testgroup0");
 				getClient(A).getOWLOntologyIDs(groups,
 						new OnOntologyIDsReceivedListener() {
-					@Override
-					public void onOntologyIDsGot(Result result,
-							Map<Object, Set<OWLOntologyID>> ids) {
-						System.out.println(
-								"onOntologyIDsGot("+result+", "+ids+")");
-						h.result0 = result;
-						h.objs[0] = ids;
-					}
-				});
+							@Override
+							public void onOntologyIDsGot(Result result,
+									Map<Object, Set<OWLOntologyID>> ids) {
+								System.out.println("onOntologyIDsGot(" + result
+										+ ", " + ids + ")");
+								h.result0 = result;
+								h.objs[0] = ids;
+							}
+						});
 				Thread.sleep(5000);
 				getClient(B).getOWLOntologyIDs(groups,
 						new OnOntologyIDsReceivedListener() {
-					@Override
-					public void onOntologyIDsGot(Result result,
-							Map<Object, Set<OWLOntologyID>> ids) {
-						h.result1 = result;
-						h.objs[1] = ids;
-					}
-				});
+							@Override
+							public void onOntologyIDsGot(Result result,
+									Map<Object, Set<OWLOntologyID>> ids) {
+								h.result1 = result;
+								h.objs[1] = ids;
+							}
+						});
 			} catch (FetchException e) {
 				e.printStackTrace();
 			} catch (AddException e) {
@@ -314,16 +545,11 @@ public class ClientTestCase extends AbstractApplicationTestCase {
 			}
 		}
 	}
-	
+
 	@Test
-	public void testGetOntologyIDs() throws InterruptedException, FetchException,
-			OWLOntologyCreationException, AddException {
+	public void testGetOntologyIDs() throws InterruptedException,
+			FetchException, OWLOntologyCreationException, AddException {
 		System.out.println("------testGetOntologyIDs()------start");
-//		clearHolder();
-//		h.objs = new Object[2];
-//		AddOntoThread addOntoThread = new AddOntoThread();
-//		addOntoThread.start();
-//		Thread.sleep(20000);
 		clearHolder();
 		h.objs = new Object[2];
 		GetOntoIDsThread getOntoIDsThread = new GetOntoIDsThread();
@@ -337,13 +563,13 @@ public class ClientTestCase extends AbstractApplicationTestCase {
 		System.out.println("h.objs[1]=" + h.objs[1]);
 		System.out.println("------testGetOntologyIDs()------end\n");
 	}
-	
+
 	private void clearHolder() {
 		h.objs = null;
 		h.result0 = null;
 		h.result1 = null;
 	}
-	
+
 	// @Test
 	// public void testAddEmptySharedOWLOntology()
 	// throws OWLOntologyCreationException, InterruptedException,
